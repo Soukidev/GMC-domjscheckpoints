fa282c5dedf6052534c9a13ea8e11c1c1073cf53
Soukidev <soukainamouradi58@gmail.com>
2024-12-22 15:08:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('colorButton'); // Get the button element
+var box = document.getElementById('changeColor'); // Get the box element
+
+button.onclick = function changecolor() {
+    box.style.background = 'blue'; // Change the background color to blue
+};
